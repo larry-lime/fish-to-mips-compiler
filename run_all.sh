cb1d09d39dc32b541fb8a2b16f3818d63d9633f9
Lawrence Lim <ll4715@nyu.edu>
2024-02-21 17:15:03 -0500

    feat: modified test runner to lex and parse .fish files

--- run_all.sh
+++ run_all.sh
@@ -3,5 +3,6 @@ cd test
 FILES=$( (ls *.fish))
 for f in $FILES; do
 	echo "Compiling: $f"
-	../ps3 sexp ${f%.fish}.sexp >../compiled/${f%.fish}.s
+	# ../ps3 sexp ${f%.fish}.sexp >../compiled/${f%.fish}.s
+	../ps3 src ${f%.fish}.fish >../compiled/${f%.fish}.s
 done
